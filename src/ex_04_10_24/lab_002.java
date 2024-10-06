3aa5d487653ea36ca3347bccf378e61a116d18b7
pavann <pavankalyanreddy431@gmail.com>
2024-10-07 00:35:44 +0530

    all files

---
+++ src/ex_04_10_24/lab_002.java
@@ -0,0 +1,4 @@
+package ex_04_10_24;
+
+public class lab_002 {
+}
---
+++ src/ex_04_10_24/lab_01.java
@@ -0,0 +1,7 @@
+package ex_04_10_24;
+
+public class lab_01 {
+    public static void main(String [] args){
+        System.out.println("10*1=10");
+    }
+}
